a0853db48f512562e5a11930b4e6478b565030e0
Brent <BrentC0328@gmail.com>
2023-12-08 18:52:44 -0500

    refactored Java models. Abstract class for animals to clean up repeat code

--- src/main/java/Brent/Cubbbage/PracticeSpringCRUD/Model/Dog.java
+++ src/main/java/Brent/Cubbbage/PracticeSpringCRUD/Model/Dog.java
@@ -3,49 +3,7 @@ package Brent.Cubbbage.PracticeSpringCRUD.Model;
 import javax.persistence.*;
 
 @Entity
-@Table
-public class Dog {
+public class Dog extends Animal {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String name;
-
-    private String color;
-
-    public Dog() {
-    }
-
-    public Dog(Long id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
 
