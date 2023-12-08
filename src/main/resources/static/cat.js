3eab5d0df38c3e43f9a1c72be964aa4f8ed478ef
Brent <BrentC0328@gmail.com>
2023-12-07 21:40:21 -0500

    all generate. Implementing method to remove one animal on a collision. Currently errors out once a collision occurs

--- src/main/resources/static/cat.js
+++ src/main/resources/static/cat.js
@@ -69,32 +69,63 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
 
-        catImages.forEach(animal => {
+  function draw() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
 
-            animal.image.src = 'cat.png';
-            context.drawImage(animal.image, animal.x, animal.y, 50, 50);
+      catImages.forEach(animal => {
 
-            //This part grabs takes the name of the animal and places it on top of the picture.
-            context.font = "12px Arial";
-            context.fillStyle = "#ffffff";
-            context.fillText(animal.name, animal.x, animal.y - 5);
+          animal.image.src = 'cat.png';
+          context.drawImage(animal.image, animal.x, animal.y, 50, 50);
 
-            animal.x += animal.speedX;
-            animal.y += animal.speedY;
+          //This part grabs takes the name of the animal and places it on top of the picture.
+          context.font = "12px Arial";
+          context.fillStyle = "#ffffff";
+          context.fillText(animal.name, animal.x, animal.y - 5);
 
-            // Bounce off walls
-            if (animal.x < 0 || animal.x + 50 > canvas.width) {
-                animal.speedX *= -1;
-            }
+          animal.x += animal.speedX;
+          animal.y += animal.speedY;
 
-            if (animal.y < 0 || animal.y + 50 > canvas.height) {
-                animal.speedY *= -1;
-            }
-        });
+          // Bounce off walls
+          if (animal.x < 0 || animal.x + 50 > canvas.width) {
+              animal.speedX *= -1;
+          }
 
-        requestAnimationFrame(draw);
+          if (animal.y < 0 || animal.y + 50 > canvas.height) {
+              animal.speedY *= -1;
+          }
+
+          // Check for collisions with other animals
+          catImages.forEach(otherAnimal => {
+              if (animal !== otherAnimal && checkCollision(animal, otherAnimal)) {
+                  // 50/50 chance to remove either animal
+                  if (Math.random() < 0.5) {
+                      removeAnimal(animal);
+                  } else {
+                      removeAnimal(otherAnimal);
+                  }
+              }
+          });
+      });
+
+      requestAnimationFrame(draw);
+  }
+  });
+
+ //Going to use a checkCollision function to check if two animal pictures run into one another.
+    //This will allow the implementation of removing one when they "crash"
+    function checkCollision(animal1, animal2) {
+        return (
+            animal1.x < animal2.x + 50 &&
+            animal1.x + 50 > animal2.x &&
+            animal1.y < animal2.y + 50 &&
+            animal1.y + 50 > animal2.y
+        );
     }
-});
+
+    function removeAnimal(animal) {
+        const index = catImages.indexOf(animal);
+        if (index !== -1) {
+            catImages.splice(index, 1);
+        }
+    }
