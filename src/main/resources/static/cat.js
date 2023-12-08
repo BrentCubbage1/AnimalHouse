29a509b37e9985cbf7c25fa70b00d8b31571c644
Brent <BrentC0328@gmail.com>
2023-12-07 20:45:30 -0500

    Letting animals out now generates sprites for each cat and dog. Still using one image cat.png for both types

--- src/main/resources/static/cat.js
+++ src/main/resources/static/cat.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('catCanvas');
     const context = canvas.getContext('2d');
@@ -6,77 +5,89 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let catImages = [];
 
+    startMovementButton.addEventListener('click', function () {
+        // Fetch both cats and dogs concurrently
+        Promise.all([fetchData("/cat/read"), fetchData("/dog/read")])
+            .then(([catResponse, dogResponse]) => {
+                // Process cat data
+                console.log(catResponse);
+                console.log(dogResponse);
+                catImages = catResponse.map(catData => {
+                    return {
+                        x: Math.random() * canvas.width,
+                        y: Math.random() * canvas.height,
+                        speedX: (Math.random() - 0.5) * 2,
+                        speedY: (Math.random() - 0.5) * 2,
+                        image: new Image(),
+                    };
+                });
 
-    //On button click, we...
-   startMovementButton.addEventListener('click', function () {
+                // Process dog data
+                const dogImages = dogResponse.map(dogData => {
+                console.log(dogData);
+                    return {
+                        x: Math.random() * canvas.width,
+                        y: Math.random() * canvas.height,
+                        speedX: (Math.random() - 0.5) * 2,
+                        speedY: (Math.random() - 0.5) * 2,
+                        image: new Image(),
+                    };
+                });
 
-      //Similar to the readAll, we GET the cats from the DB using our API call.
-           $.ajax({
-               type: "GET",
-               crossDomain: true,
-               headers: {
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json',
-                   'Access-Control-Allow-Origin': '*'
-               },
-               url: "/cat/read",
-               //On successful call, we start the function
-               success: function (response) {
+                // Combine cat and dog images
+                catImages = catImages.concat(dogImages);
+                console.log(catImages);
 
-               //Empty our cat Images, so we don't dupilcate cats.
-                   catImages = [];
+                // Start animation
+                requestAnimationFrame(draw);
+            })
+            .catch(error => {
+                console.log("Error fetching data:", error);
+            });
+    });
 
-               //Loop, for each cats data we get out of the call, we setup the image and speeds.
-               //Then put the picture into our catImages holder.
-                   response.forEach(catData => {
-                       const cat = {
-                           x: Math.random() * canvas.width,
-                           y: Math.random() * canvas.height,
-                           speedX: (Math.random() - 0.5) * 2,
-                           speedY: (Math.random() - 0.5) * 2,
-                           image: new Image(),
-                       };
-                       cat.image.src = 'cat.png';
-                       catImages.push(cat);
-                   });
+    function fetchData(endpoint) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: "GET",
+                crossDomain: true,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                url: endpoint,
+                success: function (response) {
+                    resolve(response);
+                },
+                error: function (request, status, error) {
+                    reject(error);
+                }
+            });
+        });
+    }
 
-               // requestAnimationFrame is a method in the Window object in JS.
-               // used to schedule the function before the next 'repaint' of the browser window.
-               // Will continuously call the 'draw' function so it will update the images positions.
+    function draw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-                   requestAnimationFrame(draw);
-               },
-               error: function (request, status, error) {
-                   console.log("Error while fetching cat data");
-                   console.log("Request value ↓");
-                   console.log(request);
-                   console.log("Status value ↓");
-                   console.log(status);
-                   console.log("Error value ↓");
-                   console.log(error);
-               }
-           });
-       });
+        catImages.forEach(animal => {
 
-       function draw() {
-           context.clearRect(0, 0, canvas.width, canvas.height);
+            animal.image.src = 'cat.png';
+            context.drawImage(animal.image, animal.x, animal.y, 50, 50);
 
-           catImages.forEach(cat => {
-               context.drawImage(cat.image, cat.x, cat.y, 50, 50);
+            animal.x += animal.speedX;
+            animal.y += animal.speedY;
 
-               cat.x += cat.speedX;
-               cat.y += cat.speedY;
+            // Bounce off walls
+            if (animal.x < 0 || animal.x + 50 > canvas.width) {
+                animal.speedX *= -1;
+            }
 
-               // Bounce off walls
-               if (cat.x < 0 || cat.x + 50 > canvas.width) {
-                   cat.speedX *= -1;
-               }
+            if (animal.y < 0 || animal.y + 50 > canvas.height) {
+                animal.speedY *= -1;
+            }
+        });
 
-               if (cat.y < 0 || cat.y + 50 > canvas.height) {
-                   cat.speedY *= -1;
-               }
-           });
-
-           requestAnimationFrame(draw);
-       }
-   });
+        requestAnimationFrame(draw);
+    }
+});
